cbad773142fa2b11960f4e595d5993c0808efa25
sachinkrushnaraobhorkar <101672721+sachinkrushnaraobhorkar@users.noreply.github.com>
2022-04-08 09:50:54 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 String s3 = t.toString();
                 String t1= "Register here";
                 String s4 = t1.toString();
-                if(s1.contentEquals("vishal") && s2.contentEquals("1234")){
+                if(s1.contentEquals("Admin") && s2.contentEquals("Admin")){
                     Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
                     ts.speak(s3,TextToSpeech.QUEUE_ADD,null,null);
 
@@ -57,51 +57,5 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     }
 
 
-  /*
-    EditText e1;
-    Button b1;
-    String s1;
-    TextToSpeech ts;
-
-    @Override
-    protected void onCreate( @Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        e1= findViewById(R.id.edit1);
-        b1 = findViewById(R.id.click);
-        ts= new TextToSpeech(MainActivity.this,this);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "this is sachin ", Toast.LENGTH_LONG).show();
-                s1= e1.getText().toString();
-                ts.speak(s1,TextToSpeech.QUEUE_ADD , null);
-            }
-        });
-    }
-
-    @Override
-    public void onInit(int i) {
-       ts.setLanguage(Locale.ENGLISH);
-       ts.setSpeechRate(0.8f);
-
-    }
-
-
-
- Button b1,b2 ;
-    @Override
-    protected void onCreate( @Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1= findViewById(R.id.login);
-        b2 = findViewById(R.id.register);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, " this is sachin ", Toast.LENGTH_LONG).show();
-            }
-        });
-    }*/
-
+  
     }
